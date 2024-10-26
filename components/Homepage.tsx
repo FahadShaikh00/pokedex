56f2b18c432444625791c14773020a224ebdd99b
Fahad Shaikh <109937241+FahadShaikh00@users.noreply.github.com>
2024-10-26 22:23:17 +0530

    Added Components & Dependencies
    Added Components
    - Homepage - which contains Search bar and Pokemon list
    - PokemonDetails - will contain details when User click on a Pokemon Card
    
    Installed Dependencies
    - react-native-web
    - react-dom
    - @expo/metro-runtime

---
+++ components/Homepage.tsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export default function Homepage() {
+    return <Text>Welcome to Pokédex</Text>
+}
---
+++ components/PokemonDetails.tsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export default function Homepage() {
+    return <Text>Pokemon Details</Text>
+}
