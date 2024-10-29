61a6d1b9a77e46cdbd94e3d4184590e04faef35d
Fahad Shaikh <109937241+FahadShaikh00@users.noreply.github.com>
2024-10-29 09:29:39 +0530

    feat: add navigation
    Added navigation between two screens Homepage and PokemonDetails

--- App.tsx
+++ App.tsx
@@ -1,13 +1,33 @@
-import { Text, View } from 'react-native';
-import Homepage from './components/Homepage';
-import PokemonDetails from './components/PokemonDetails';
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Homepage from "./components/Homepage";
+import PokemonDetails from "./components/PokemonDetails";
 
-export default function App() {
+export type RootStackParamList = {
+  Home: undefined;
+  PokeDetails: any;
+};
 
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const App = () => {
   return (
-    <View>
-      <Homepage />
-      <PokemonDetails />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Homepage} />
+        <Stack.Screen name="PokeDetails" component={PokemonDetails} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    // flex: 1,
+    // backgroundColor: "#fff",
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+});
+export default App;
