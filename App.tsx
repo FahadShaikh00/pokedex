52893fb8c9961246e45ca6aa66021a7f0e831583
Fahad Shaikh <109937241+FahadShaikh00@users.noreply.github.com>
2024-10-31 09:04:09 +0530

    feat: add header logo and title
    - Added Pokeball Logo in Header along with Pokedex Title

--- App.tsx
+++ App.tsx
@@ -1,16 +1,36 @@
-import { StyleSheet } from "react-native";
+import { Image, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import Homepage from "./components/Homepage";
 import PokemonDetails from "./components/PokemonDetails";
+import PokemonList from "./components/PokemonList";
 
 export type RootStackParamList = {
-  Home: undefined;
+  Home: any;
   PokeDetails: any;
+  PokeList: any;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const Header = () => (
+  <View
+    style={{
+      flex: 1,
+      flexDirection: "row",
+      alignItems: "center",
+    }}
+  >
+    <Image
+      style={{ width: 50, height: 36, resizeMode: "contain" }}
+      source={require("./assets/headerLogo.png")}
+    />
+    <Text style={{ fontSize: 36, fontWeight: "bold", color: "white" }}>
+      Pokédex
+    </Text>
+  </View>
+);
 
 const App = () => {
   return (
@@ -20,21 +40,25 @@ const App = () => {
           <Stack.Screen
             name="Home"
             component={Homepage}
-            options={{ title: "Pokédex" }}
+            options={{
+              headerTitle: () => <Header />,
+              headerTransparent: true,
+            }}
           />
-          <Stack.Screen name="PokeDetails" component={PokemonDetails} />
+          <Stack.Screen
+            name="PokeDetails"
+            component={PokemonDetails}
+            options={{ headerTransparent: true }}
+          />
+          <Stack.Screen
+            name="PokeList"
+            component={PokemonList}
+            options={{ headerTitle: "Pokemon List" }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    // flex: 1,
-    // backgroundColor: "#fff",
-    // alignItems: "center",
-    // justifyContent: "center",
-  },
-});
 export default App;
